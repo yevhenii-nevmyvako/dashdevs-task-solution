4d01d63358cb2b8c3d83dcde7f65d83beb4a305d
Yevhenii <yevhenii.nevmyvako@gmail.com>
2024-06-03 17:57:38 +0300

    Add definition with solution task find_index.

---
+++ equilibrium_index_finder/core/base.py
@@ -0,0 +1,45 @@
+import json
+
+from equilibrium_index_finder.validations.validation_input_array import validate_input_arr
+
+_NOT_EQUUILIBRIUM_INDEX = -1
+
+
+def save_result_to_json(result_data: int, dst_filepath: str) -> None:
+    """Saves result to json.
+
+    Args:
+        result_data: Equilibrium index.
+        dst_filepath: Path to destination file.
+
+    """
+
+    with open(dst_filepath, 'w') as f:
+        json.dump({"equilibrium_index": result_data}, f, indent=2)
+
+
+def find_index(arr: list) -> int:
+    """Algorithm to find the equilibrium index in list of integers
+
+    Args:
+        arr: Input list integers.
+
+
+    Returns:
+        Equilibrium index, or (-1) if it's not in the list.
+
+    """
+    validate_input_arr(arr)
+
+    total_sum = sum(arr)
+    left_sum = 0
+
+    for index in range(len(arr)):
+        total_sum -= arr[index]
+
+        if left_sum == total_sum:
+            return index
+
+        left_sum += arr[index]
+
+    return _NOT_EQUUILIBRIUM_INDEX
